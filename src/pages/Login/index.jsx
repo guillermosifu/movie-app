7519ddc252cbd182c1a4b4b227993e5944124bbf
guillermosifu <sifuentes1201@gmail.com>
2022-12-06 21:53:09 -0500

    update

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,52 @@
+import { Avatar } from "@mui/material";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+
+
+
+const Login =()=>{
+    return(
+        <Container maxWidth="xs">
+          <CssBaseline/>
+          <Box sx={{marginTop:8,
+        display:"flex", flexDirection:"column",alignItems:"center"}}>
+        <Avatar sx={{m:2,bgcolor:"red"}}>
+            {/* falta el icon */}
+        </Avatar>
+        <Typography component ="h1" variant="h5">
+            Sign In
+        </Typography>
+        <Box>
+            <TextField
+            margin= "normal"
+            label="User"
+            fullWidth
+            autoFocus
+            id="User"/>
+
+             <TextField
+             id="password"
+            margin= "normal"
+            label="Password"
+            type="password"
+            fullWidth
+            autoFocus/>
+            {/* <FormControlLabel
+            control={<Checkbox color="green"/>}
+            label="Remember me"/> */}
+            <Button type="submit" fullWidth variant="contained">
+                Login
+            </Button>
+        </Box>
+          </Box>
+        </Container>
+    )
+}
+
+export default Login;
